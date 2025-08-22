34eae906824901baa3907f0ba02dd8b132e3459c
TTTu-72 <tsuttuu@gmail.com>
2025-08-23 07:20:26 +0900

    modify extension icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  modules: ['@wxt-dev/module-solid'],
+  modules: ['@wxt-dev/module-solid', '@wxt-dev/auto-icons'],
   manifest: {
     name: 'YouTube Screenshot',
     description: 'YouTubeの動画にスクリーンショット機能を追加します',
